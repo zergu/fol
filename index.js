8496e40e7e3c5cd0d3a24cd748e7118430789114
zergu <michal.zagdan@gmail.com>
2021-11-17 21:35:13 +0100

    reorg

--- index.js
+++ index.js
@@ -1,100 +1,50 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
 const sqlite3 = require('sqlite3');
 
-const profiles = [
-  'https://twitter.com/orion_money',
-  'https://twitter.com/kartracingnft',
-  'https://twitter.com/ensdomains',
-  'https://twitter.com/OlympusDAO',
-  'https://twitter.com/CryptoVsZombie',
-  'https://twitter.com/MakerDAO',
-  'https://twitter.com/WarenaOfficial',
-  'https://twitter.com/demoleio',
-  'https://twitter.com/ProjectSeedGame',
-  'https://twitter.com/TheSandboxGame',
-  'https://twitter.com/GuildOfGuardian',
-  'https://twitter.com/PlayEmberSword',
-];
-
-/**
- * TODO:
- * - cron
- * - data presentation
- */
-
-const db = new sqlite3.Database('./followers.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./followers.db', (err) => {
   if (err) {
     console.log("Getting error " + err);
     exit(1);
   }
-});
 
-const getOrCreateProfile = function (url) {
-  return new Promise((resolve, reject) => {
-    db.get(`SELECT rowid FROM profile WHERE url = '${url}'`, function (err, row) {
-      if (row) {
-        resolve(row.rowid);
+  console.log('RESULTS:');
+
+  const getResults = function (callback) {
+    db.all(`
+        SELECT p.name, s.day, s.followers FROM profile p
+        JOIN profile_stats s ON p.rowid = s.profile_id
+        ORDER BY p.name, s.day ASC`, (err, rows) => {
+      callback(rows);
+
+    });
+  }
+
+  const build = function (rows) {
+    let projects = {};
+    
+    rows.forEach((row) => {
+      if (row.name in projects) {
+        projects[row.name]['days'] += 1;
+        projects[row.name]['max_followers'] = row.followers;
       } else {
-        db.run(`INSERT INTO profile (url) VALUES ('${url}')`, (err) => {
-          if (err) {
-            console.error(err);
-          } else {
-            resolve(this.lastId);
-          }
-        });
-      }
-    });
-  });
-}
-
-const hasStats = function (profileId, date) {
-  return new Promise((resolve, reject) => {
-    db.get(`SELECT rowid FROM profile_stats WHERE profile_id = ${profileId} AND day = '${date}'`, (err, row) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      }
-      resolve(row !== undefined);
-    });
-  })
-}
-
-const getTodaysDate = function () {
-  const today = new Date();
-
-  return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-}
-
-const getStats = function (profileId, url) {
-  request(url)
-    .then(function (html) {
-      const $ = cheerio.load(html);
-      const followers = $('a[data-nav="followers"]').attr('title').replace(/\D/g, '');
-      const name = $('a.ProfileHeaderCard-nameLink').text();
-      db.exec(`UPDATE profile SET name = '${name}' WHERE rowid = ${profileId}`);
-      db.exec(`INSERT INTO profile_stats (profile_id, day, followers) VALUES (${profileId}, '${getTodaysDate()}', '${followers}')`);
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
-}
-
-const sleep = (seconds) => {
-  return new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000));
-};
-
-/* Main */
-
-profiles.forEach(function (url, index) {
-  getOrCreateProfile(url).then((profileId) => {
-    hasStats(profileId, getTodaysDate()).then((result) => {
-      if (!result) {
-        getStats(profileId, url);
+        projects[row.name] = {
+          days: 0,
+          min_followers: row.followers
+        }
       }
     })
-  });
+
+    Object.entries(projects).forEach(([key, row]) => {
+      const avg = (row.max_followers - row.min_followers) / row.days;
+      projects[key]['avg'] = avg;
+      projects[key]['percent_increase'] = (avg / row.max_followers * 100).toFixed(2) + '%';
+    });
+    
+    Object.entries(projects).forEach(([key, row]) => {
+      console.log(key + ': ' + row.percent_increase);
+    });
+  }
   
-  sleep(2);
+  getResults(build);
 });
+
 
